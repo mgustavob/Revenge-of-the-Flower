34522572fb1dc865cb870d9b6f921600645b9206
Martin Briceno <martin.briceno@live.com>
2020-07-28 11:43:33 -0400

    snake moves automatically and follow direction of arrow

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-
-
-  document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     // console.log('Dom loaded')
-     // DOM REFS
-     movementDisplay = document.getElementById('movement');
-     game = document.getElementById('game');
-     // CANVAS CONFIG
-     game.setAttribute('height', 400);
-     game.setAttribute('width', 800);
-     ctx = game.getContext('2d');
-     // CHARACTER REFS
-     ogre = new Crawler(300, 100, 80, 120, '#bada55');
-     hero = new Crawler(200, 100, 50, 50, 'hotpink');
-     document.addEventListener('keydown', movementHandler);
-     let runGame = setInterval(gameLoop, 60);
-   })
+    // DOM REFS
+    movementDisplay = document.getElementById('movement');
+    game = document.getElementById('game');
+    // CANVAS CONFIG
+    game.setAttribute('height', 400);
+    game.setAttribute('width', 500);
+    ctx = game.getContext('2d');
+    // CHARACTER REFS
+    food = new Crawler(100, 100, 20, 20, '#bada55');
+    snake = new Crawler(20, 20, 20, 20, 'hotpink');
+    document.addEventListener('keydown', ((e) => {
+       // console.log(e);
+        const direct = e.keyCode;
+        snake.newDirec(direct);
+    }));
+    window.setInterval(() => {
+        // thi is so that the snake doesnt live a trail unless he eats the apple
+        ctx.clearRect(0, 0, game.width, game.height);
+        snake.update();
+        snake.render();
+        food.render();
+    }, 200);
+});
--- js/snake.js
+++ js/snake.js
@@ -3,60 +3,49 @@ function Crawler(x, y, width, height, color) {
     this.y = y;
     this.width = width;
     this.height = height;
+    this.xSpeed = 10;
+    this.ySpeed = 0;
     this.color = color;
     this.alive = true;
     this.render = function() {
-      ctx.fillStyle = this.color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-  }
+    this.update = function () {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+    }
+    this.newDirec = function(direct) {
+        switch (direct) {
+            case (38):
+                this.xSpeed = 0;
+                this.ySpeed = -10;
+                break;
+            case (40):
+                this.xSpeed = 0;
+                this.ySpeed = 10;
+                break;
+            case (37):
+                this.xSpeed = -10;
+                this.ySpeed = 0;
+                break;
+            case (39): 
+                this.xSpeed = 10;
+                this.ySpeed = 0;
+                break;
+          default:
+            console.log('invalid keystroke');
+        }
+      }
+}
 
-  const detectHit = () => {
-      //check for colission on x axis
-      if (hero.x + hero.width > ogre.x &&
-          hero.x < ogre.x + ogre.width &&
-          hero.y + hero.height > ogre.y &&
-          hero.y < ogre.y + ogre.height) {
+const detectHit = () => {
+    //check for colission on x axis
+    if (snake.x + snake.width > food.x &&
+        snake.x < food.x + food.width &&
+          snake.y + snake.height > food.y &&
+          snake.y < food.y + food.height) {
           console.log('collision');
-          ogre.alive = false;
+          food.alive = false;
       }
   }
-
-  const gameLoop = () => {
-    // console.log('looping in 💩')
-    // clear the cavas
-    ctx.clearRect(0, 0, game.width, game.height);
-    // display the x, y coordinates of our hero onto the DOM
-    movementDisplay.textContent = `X:${hero.x}\nY:${hero.y}`;
-    // check if the ogre is alive and
-    if (ogre.alive) {
-      // render the ogre
-      ogre.render()
-      // check for collision
-      // TODO detectHit()
-      detectHit();
-    }
-    // render the hero
-    hero.render()
-  }
-  const movementHandler = e => {
-   // console.log(e)
-    // w: 87, a:65, s:83, d:68
-    switch (e.keyCode) {
-      case (87): // w up
-        if (hero.y > 0) hero.y -=5
-        break;
-      case (83): // s down
-        if (hero.y + hero.height < game.height) hero.y +=5
-        break;
-      case (65): // a left
-        if (hero.x > 0) hero.x -=5
-        break;
-      case (68): // d right
-        if (hero.x + hero.width < game.width) hero.x +=5
-        break;
-      default:
-        console.log('invalid keystroke');
-    }
-  }
-  
